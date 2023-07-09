dfc4b6b0cc65d448154d2b0261d329bc312c470b
1Molot <sinkevich1990@gmail.com>
2023-07-09 19:12:20 +0300

    fix:Time

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ContextProvider} from "./reducer";
+import {Timer} from "./Timer";
+import {render} from "@testing-library/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const App = document.getElementById("app");
+render(
+    <ContextProvider>
+        <Timer/>
+    </ContextProvider>,
+    App
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
